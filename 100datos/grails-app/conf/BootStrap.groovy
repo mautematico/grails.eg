8e66b0b543c6bc57fd99a005202ae4aa33a9adf0
mautematico <mauricio@navarromiranda.mx>
2015-05-27 12:59:26 -0500

    Bootstrap no necesita cargar todas las personas, se cargan automáticamente.

--- 100datos/grails-app/conf/BootStrap.groovy
+++ 100datos/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,15 @@
+import com.mx.eglobal.Persona;
 class BootStrap {
 
     def init = { servletContext ->
+/*    	def personas = Persona.getAll()
+
+    	personas.each{
+    		it.save(failOnError:true);
+    		println it;
+
+    	}
+*/
     }
     def destroy = {
     }
