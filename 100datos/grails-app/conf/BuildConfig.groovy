d62305ae3214c80c49373fe8751041d435693f73
JessicaMatiasEG <iscmatias@outlook.com>
2015-06-12 14:49:37 -0500

    se agrego el plugin de export

--- 100datos/grails-app/conf/BuildConfig.groovy
+++ 100datos/grails-app/conf/BuildConfig.groovy
@@ -44,6 +44,7 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo "http://repo.grails.org/grails/core"
     }
 
     dependencies {
@@ -52,6 +53,7 @@ grails.project.dependency.resolution = {
         // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
         test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
         runtime "postgresql:postgresql:9.1-901.jdbc4" 
+        compile 'commons-beanutils:commons-beanutils:1.8.3'
      }
 
     plugins {
@@ -62,6 +64,7 @@ grails.project.dependency.resolution = {
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.8'
         compile ":asset-pipeline:1.9.9"
+        compile ":export:1.6"
 
     //	compile ":datasources:0.5"
 
